0864d749ff0db45fe61e2ae323b73a3346116bd9
Agustin Bergalli <70240355+agusbergalli@users.noreply.github.com>
2021-12-02 22:13:53 -0300

    Revert "idiomas v 1.0"
    This reverts commit 9f1d104335d44742fbfad980a082ee7cbba8d6ed.

--- js/idiomas.js
+++ js/idiomas.js
@@ -1,32 +1,7 @@
-function cambiarIdioma(id){//voy a cambiar a el idioma que falta
+function espaniolYportugues(){}
 
-    if (document.getElementById("espaniol") === null){//significa que falta español
-        agregarEspañol(id);
-    } 
+function inglesYportugues(){}
 
-    if (document.getElementById("portugues") === null){//significa que falta portugues
-        agregarPortugues(id);
-    } 
-    if (document.getElementById("ingles") === null){//significa que falta ingles
-        agregarIngles(id);
-    } 
-
-}
-
-function agregarEspañol(identificador){
-    document.getElementById(identificador).src = "images/espania.jpg";
-    document.getElementById(identificador).title = "Español";
-    document.getElementById(identificador).id = "espaniol"; 
-}
-
-function agregarPortugues(identificadorP){
-    document.getElementById(identificadorP).src = "images/Brasil.png";
-    document.getElementById(identificadorP).title = "Português";
-    document.getElementById(identificadorP).id = "portugues";  
-}
-
-function agregarIngles(identificadorI){
-    document.getElementById(identificadorI).src = "images/gc597b02fc70a409cc9cc2917942426f1de33173d7a4b18b4c927dce3c57aa933404a5b09e512eba56b6b713cb3dfbf0c_1280.png";
-    document.getElementById(identificadorI).title = "English";
-    document.getElementById(identificadorI).id = "ingles"; 
+function inglesYespaniol(){//Selecciona el idioma portugues y lo oculta
+    document.getElementById("portugues").src = "images/espania.jpg"
 }
