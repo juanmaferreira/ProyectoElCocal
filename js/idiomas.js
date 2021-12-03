c61daa8578a353f62cd8d9a49b669bcc399dcb05
juanma <70178226+juanmaferreira@users.noreply.github.com>
2021-12-02 21:15:37 -0300

    previa idiomas

---
+++ js/idiomas.js
@@ -0,0 +1,7 @@
+function espaniolYportugues(){}
+
+function inglesYportugues(){}
+
+function inglesYespaniol(){//Selecciona el idioma portugues y lo oculta
+    document.getElementById("portugues").src = "images/espania.jpg"
+}
