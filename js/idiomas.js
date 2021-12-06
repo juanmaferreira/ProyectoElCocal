9c7ebd5e9fcb59cbdc043a5166896a605eaba83f
juanma <70178226+juanmaferreira@users.noreply.github.com>
2021-12-06 20:24:13 -0300

    Version 2 del proyecto

--- js/idiomas.js
+++
@@ -1,7 +0,0 @@
-function espaniolYportugues(){}
-
-function inglesYportugues(){}
-
-function inglesYespaniol(){//Selecciona el idioma portugues y lo oculta
-    document.getElementById("portugues").src = "images/espania.jpg"
-}
