164d1f3512ce4b3136bcd373ec0acedda756765e
Agustin Bergalli <70240355+agusbergalli@users.noreply.github.com>
2021-12-17 13:29:17 -0300

    Update tarifasIdioma.js

--- js/tarifasIdioma.js
+++ js/tarifasIdioma.js
@@ -138,7 +138,7 @@ function cambiarAPortugues(){
     <a href="Instalaciones.html#sec-407e" data-page-id="76424729" class="u-active-grey-40 u-border-none u-btn u-btn-round u-button-style u-grey-25 u-hover-grey-30 u-radius-50 u-btn-8">VER SERVIÇOS
     </a>`;
 document.getElementById("bloque2").innerHTML = `
-    <h2 class="u-text u-text-default u-text-1">Taxas de Carpicabaña</h2>
+    <h2 class="u-text u-text-default u-text-1">Taxas de carpicabaña</h2>
     <p class="u-text u-text-2"> Época 2021
     <br> O preço está em pesos uruguaios.
     </p>
@@ -351,7 +351,7 @@ function cambiarAIngles(){
     <a href="Instalaciones.html#sec-407e" data-page-id="76424729" class="u-active-grey-40 u-border-none u-btn u-btn-round u-button-style u-grey-25 u-hover-grey-30 u-radius-50 u-btn-8">SEE SERVICES
     </a>`;
 document.getElementById("bloque2").innerHTML = `
-    <h2 class="u-text u-text-default u-text-1">Prices of Carpicabañas</h2>
+    <h2 class="u-text u-text-default u-text-1">Prices of carpicabañas</h2>
     <p class="u-text u-text-2"> Season 2021
     <br>The price is in Uruguayan pesos.
     </p>
